4f936a7158e0c6a0ad6afe69cf8321bb585c8631
kfyty <kfyty725@hotmail.com>
2021-04-20 20:45:24 +0800

    active & include

--- dynamic-data-source-demo/src/main/java/com/kfyty/dynamic/DynamicDataSourceApplication.java
+++ dynamic-data-source-demo/src/main/java/com/kfyty/dynamic/DynamicDataSourceApplication.java
@@ -1,7 +1,10 @@
 package com.kfyty.dynamic;
 
 import com.kfyty.dynamic.mapper.test.TestMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
@@ -10,10 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Slf4j
 @RestController
 @SpringBootApplication
-public class DynamicDataSourceApplication {
+public class DynamicDataSourceApplication implements CommandLineRunner {
     private static DynamicDataSourceApplication self;
+
+    @Value("${server.log}")
+    private Boolean serverLog;
+
+    @Value("${server.config}")
+    private String serverConfig;
 
     @Autowired
     private TestMapper testMapper;
@@ -32,5 +42,11 @@ public class DynamicDataSourceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DynamicDataSourceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("server log: {}", serverLog);
+        log.info("server config: {}", serverConfig);
     }
 }
